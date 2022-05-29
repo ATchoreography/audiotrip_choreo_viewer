2804a5091d04ac2d04a6b0cbcb7756340a43939d
Davide Depau <davide@depau.eu>
2022-05-29 18:58:17 +0200

    Work around choreos with broken tempo sections

--- src/audiotrip/utils.cpp
+++ src/audiotrip/utils.cpp
@@ -8,6 +8,15 @@
 namespace audiotrip {
 
 std::vector<Beat> AudioTripSong::computeBeats() const {
+  // Find the max beat used in the actual choreo since some choreos have out-of-bounds beats
+  ssize_t maxBeat = 0;
+  for (const Choreography &choreo : choreographies) {
+    for (const ChoreoEvent &event : choreo.events) {
+      if (event.time.beat > maxBeat)
+        maxBeat = event.time.beat;
+    }
+  }
+
   assert(!tempoSections.empty());
   std::vector<Beat> result;
 
@@ -23,6 +32,15 @@ std::vector<Beat> AudioTripSong::computeBeats() const {
     while (accumulator < sectionEndTime) {
       result.emplace_back(accumulator, ts->beatsPerMinute);
       accumulator += secondsPerBeat;
+    }
+  }
+
+  // Always add an extra beat at the end just to be sure
+  ssize_t toAdd = (maxBeat - static_cast<ssize_t>(result.size())) + 1;
+  if (toAdd > 0) {
+    for (int i = 0; i < toAdd; i++) {
+      result.emplace_back(accumulator, tempoSections.back().beatsPerMinute);
+      accumulator += 60.f / tempoSections.back().beatsPerMinute;
     }
   }
 
